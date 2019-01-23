f6f2839d4759211567e505ac835aa6f07d27517f
roxyeurich <32582901+roxyeurich@users.noreply.github.com>
2019-01-22 20:39:34 -0800

    Update register.php

--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php
-//header('Access-Control-Allow-Origin: *'); 
+header('Access-Control-Allow-Origin: *'); 
 try {
   $conn = new PDO("mysql:host=edo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=a06uev86awzcjt3y","ofzywzaw9umo2n3o","nubyug4j44dg6i6z");
 } catch (PDOException $e){
