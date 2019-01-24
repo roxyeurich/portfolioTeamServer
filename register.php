d22016e98f3421b3bba28ed274e0cad9658f1e19
roxyeurich <32582901+roxyeurich@users.noreply.github.com>
2019-01-24 13:48:22 -0800

    Add files via upload

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *'); 
 try {
-  $conn = new PDO("mysql:host=edo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=a06uev86awzcjt3y","ofzywzaw9umo2n3o","nubyug4j44dg6i6z");
+  $conn = new PDO("mysql:host=eedo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=a06uev86awzcjt3y","ofzywzaw9umo2n3o","nubyug4j44dg6i6z");
 } catch (PDOException $e){
   echo "Error".$e->getMessage();
 }
