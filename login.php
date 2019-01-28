f06a70606e0bc969c6eb6b5f34276ca9418ff812
roxyeurich <32582901+roxyeurich@users.noreply.github.com>
2019-01-28 14:41:02 -0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
+
+try {
+  $conn = new PDO("mysql:host=edo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=a06uev86awzcjt3y","ofzywzaw9umo2n3o","nubyug4j44dg6i6z");
+} catch (PDOException $e){
+  echo "Error".$e->getMessage();
+}
+
+$username =$_POST["username"];
+$password =$_POST["password"];
+
+
+$query = "SELECT * FROM users WHERE username='$username' AND password = '$password'";
+
+$result = $conn->query($query);
+if($result){
+ echo json_encode(true); 
+}
+
+?>
